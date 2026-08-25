bdef9bd0bcfb02c329453b8ebd5435d8d0adc03b
Edumachadoh <dudu.henrique.em@gmail.com>
2026-08-24 23:48:46 -0300

    Primeiro commit

---
+++ src/tutorial_pipeline_weather/__init__.py
@@ -0,0 +1,40 @@
+import requests
+import json
+from pathlib import Path
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+api_key = '14e9176b3dc870e4fbd0504c8ac25bb0'
+url =f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={api_key}'
+
+def extract_weather_data(url:str) -> list:
+    response = requests.get(url)
+    # transformar resposta da url em dicionário python
+    data = response.json()
+    
+    if response.status_code != 200:
+        logging.error("Erro na requisição)")
+        return []
+    
+    if not data:
+        logging.info("Nenhum dado retornado")    
+        return []
+    
+    
+    output_path = 'data/weather_data.json'
+    # sair da pasta source e ir para pasta data
+    output_dir = Path(output_path).parent
+    
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    # f é o arquivo onde serão escritos os dados 
+    # with open já fecha automaticamente o arquivo após a escrita
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+        
+    return data
+
+extract_weather_data(url)
+    
